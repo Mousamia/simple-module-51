72979689261b47832e87984ff448ba36c6748a1d
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-07-07 23:27:55 +0600

    state cleared copletely

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,65 @@
 import React, { useState } from 'react';
 import State from '../State/State';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarChart, faClose, faStar } from '@fortawesome/free-solid-svg-icons';
+import { fa1 } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
+
+
 
 const Nav = () => {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [higlight, setHiglight] = useState(true);
 
     const bgChange = () => {
         setOpen(!open);
+
     }
+
+    const  myStyle8 = {
+        color: 'red',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    backgroundColor: 'lightblue',
+    }
+    const  myStyle9 = {
+        backgroundColor : 'purple',
+    }
+
+    const className = higlight ? 'myStyle8' : 'myStyle9'
+
 
     return (
         <div>
-            <ul onClick={() => bgChange()}>
+            <div onClick={bgChange}>
+
+                <span>
+                    {
+                        open === true ?
+                            <FontAwesomeIcon size='3x' icon={faClose} /> : <FontAwesomeIcon size='3x' icon={faBars} />
+                    }
+                </span>
+            </div>
+
+            <button onClick={() => setHiglight( !higlight)}> click me to chnge</button>
+
+            <ul style={higlight ? myStyle8 : myStyle9}>
                 <li>Home</li>
                 <li>Contact</li>
                 <li>Pay</li>
                 <li>Login</li>
             </ul>
-            <State></State>
-            
+
+            {/* `md:flex absolute    */}
+            {/* ` ` ` `  ` ` ` ` */}
+
+
+
+
         </div>
     );
 };
