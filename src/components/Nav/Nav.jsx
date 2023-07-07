adff91f3ad625dcebf00c46505b8308953a22766
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-07-08 01:55:43 +0600

    msg

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -20,14 +20,16 @@ const Nav = () => {
 
     }
 
-    const  myStyle8 = {
+    const myStyle8 = {
         color: 'red',
-    fontSize: '16px',
-    fontWeight: 'bold',
-    backgroundColor: 'lightblue',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        backgroundColor: 'lightblue',
+       margin : '-90px 0'
     }
-    const  myStyle9 = {
-        backgroundColor : 'purple',
+    const myStyle9 = {
+        backgroundColor: 'purple',
+        margin : '30px 0'
     }
 
     const className = higlight ? 'myStyle8' : 'myStyle9'
@@ -45,20 +47,14 @@ const Nav = () => {
                 </span>
             </div>
 
-            <button onClick={() => setHiglight( !higlight)}> click me to chnge</button>
+            <button onClick={() => setHiglight(!higlight)}> click me to chnge</button>
 
-            <ul style={higlight ? myStyle8 : myStyle9}>
+            <ul style={open ? myStyle8 : myStyle9}>
                 <li>Home</li>
                 <li>Contact</li>
                 <li>Pay</li>
                 <li>Login</li>
             </ul>
-
-            {/* `md:flex absolute    */}
-            {/* ` ` ` `  ` ` ` ` */}
-
-
-
 
         </div>
     );
